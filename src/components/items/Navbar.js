ae7307f818dcd4902eaf817e196a8f1e2baf9903
Sojiwoo <58384366+jiwooproity@users.noreply.github.com>
2022-03-26 18:53:38 +0900

    2022 03 26 / 초반 세팅

---
+++ src/components/items/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+const NavbarContainer = styled.div`
+  width: 100%;
+
+  padding: 20px;
+`;
+
+const NavbarWrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NavbarLogo = styled.h1`
+  font-size: 30px;
+`;
+
+const NavbarMenu = styled.ul``;
+
+const NavbarMenuItem = styled.li``;
+
+const Navbar = () => {
+  return <></>;
+};
+
+export default Navbar;
